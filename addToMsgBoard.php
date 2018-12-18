860c95e7c640ed72165f071992f585ecf028475f
Hamhod <hamhod@CanDER-PC.MDesktop>
2018-12-18 17:57:02 -0500

    Finished addToMsgBoard.php, Bug; seems due to chaching JS code to update message board display doesnt update with new file contents

--- addToMsgBoard.php
+++ addToMsgBoard.php
@@ -1,6 +1,9 @@
 <?php
+		// TODO: use PHP func http_special_chars func to sanitize
+
+		$msgBoardFile = "./data/msgBoard.txt";
 		$postdata = file_get_contents("php://input");	// Get POST in a JSON string
-		$messageFile = file_get_contents("./data/msgBoard.txt");	// ./ is required, specifies local filesys
+		$messageFile = file_get_contents($msgBoardFile);	// ./ is required, specifies local filesys
 
 		// conv messageFile to arr, trunc arr to last 1000 msgs
 		$messagesArr = json_decode($messageFile, true);	
@@ -11,16 +14,37 @@
 		{
 				$dataArr = json_decode($postdata);	// returns an obj from the json string
 
+				// Sanitize:
 				$newName  = preg_replace('/[^a-zA-Z ]/','',$dataArr->newName); // replace any non letter/whitespace
-				$newData = preg_replace('/[^0-9 ]/','',$dataArr->newData);	// replace any non num with space
+				$newData  = preg_replace('/[^a-zA-Z ]/','',$dataArr->newData); // replace any non letter/whitespace
+//				$newData = preg_replace('/[^0-9 ]/','',$dataArr->newData);	// replace any non num with space
 				$timeStamp = $dataArr->newTStamp;
-
 //				if($newName  == '' || $newData == '') return;	// If after sanitize empty then done
 				//				file_get_contents to read json text file, decode it into arr, add new data to arr, then
 				//				$json_data = json_encode($posts);
 				//				file_put_contents('myfile.json', $json_data);
 
-				echo $timeStamp.": ".$newName." ".$newData;
+				//echo $timeStamp.": ".$newName." ".$newData;
 		}
+		$newPost = array();
+		$newPost["TimeStamp"] = $timeStamp;
+		$newPost["Name"] = $newName;
+		$newPost["Data"] = $newData;
 
+		// Add to messages array:
+		$messagesArr[] = $newPost;
+		//var_dump($messagesArr);
+
+		$numMessages = count($messagesArr);
+		$msgCtr = 0;
+		/*
+		while($msgCtr < $numMessages)
+		{
+				echo $messagesArr[$msgCtr]["TimeStamp"].": ".$messagesArr[$msgCtr]["Name"].": ".$messagesArr[$msgCtr]["Data"]."\n";
+				$msgCtr++;
+		}
+		 */
+//		echo json_encode($messagesArr);
+		// Write JSON to message board file
+		file_put_contents($msgBoardFile, json_encode($messagesArr));
 ?>
